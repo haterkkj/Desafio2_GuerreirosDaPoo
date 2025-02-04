6cbe66d46ec530524439c70b09d67cc8edaeffbd
Thiago Ferreira dos Santos <thiagoferreira2004f@gmail.com>
2025-02-04 14:59:49 -0300

    /feat: implementing model comment and repositories for comment & post

---
+++ microservice-b/src/main/java/uol/compass/microserviceb/repositories/CommentRepository.java
@@ -0,0 +1,9 @@
+package uol.compass.microserviceb.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import uol.compass.microserviceb.model.Comment;
+
+@Repository
+public interface CommentRepository extends MongoRepository<Comment, String> {
+}
