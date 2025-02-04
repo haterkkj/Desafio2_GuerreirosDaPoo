8c7c06d80087e3f4c7825a3966b3e334621ec3ce
CaioSifuentes <caiocesar.sifuentesbarcelos@gmail.com>
2025-02-04 15:50:29 -0300

    /feat: create-post-on-controller

--- microservice-b/src/main/java/uol/compass/microserviceb/web/controller/PostController.java
+++ microservice-b/src/main/java/uol/compass/microserviceb/web/controller/PostController.java
@@ -2,12 +2,12 @@ package uol.compass.microserviceb.web.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import uol.compass.microserviceb.model.Post;
 import uol.compass.microserviceb.services.PostService;
 
+import java.net.URI;
 import java.util.List;
 
 @AllArgsConstructor
@@ -20,6 +20,16 @@ public class PostController {
     public ResponseEntity<List<Post>> getAll(){
         List<Post> listPost = service.findAll();
         return ResponseEntity.ok().body(listPost);
+    }
+
+    @PostMapping
+    public ResponseEntity<Post> create(@RequestBody Post post){
+        Post createdPost = service.save(post);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(createdPost.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(createdPost);
     }
 
 }
