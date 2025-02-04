35847e58accf24b67639037270247009e8ec4c9d
CaioSifuentes <caiocesar.sifuentesbarcelos@gmail.com>
2025-02-04 14:54:28 -0300

    /feat: post-controller-and-list-posts

---
+++ microservice-b/src/main/java/uol/compass/microserviceb/web/controller/PostController.java
@@ -0,0 +1,25 @@
+package uol.compass.microserviceb.web.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uol.compass.microserviceb.model.Post;
+import uol.compass.microserviceb.services.PostService;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+    private final PostService service;
+
+    @GetMapping
+    public ResponseEntity<List<Post>> getAll(){
+        List<Post> listPost = service.findAll();
+        return ResponseEntity.ok().body(listPost);
+    }
+
+}
