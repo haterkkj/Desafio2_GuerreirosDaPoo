ab5a43fab537566099f90e5150650bf6dd97c0a6
CaioSifuentes <caiocesar.sifuentesbarcelos@gmail.com>
2025-02-05 17:47:17 -0300

    /feat: comment-response-dto

---
+++ microservice-b/src/main/java/uol/compass/microserviceb/web/dto/CommentResponseDTO.java
@@ -0,0 +1,14 @@
+package uol.compass.microserviceb.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter @Setter
+public class CommentResponseDTO {
+    private String id;
+    private String email;
+    private String name;
+    private String body;
+}
