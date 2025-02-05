33461605fbb63b1eafb80c725ef9289236e35346
Thiago Ferreira dos Santos <thiagoferreira2004f@gmail.com>
2025-02-05 10:00:33 -0300

    /feat: implementing deleteById in service

--- microservice-b/src/main/java/uol/compass/microserviceb/services/CommentService.java
+++ microservice-b/src/main/java/uol/compass/microserviceb/services/CommentService.java
@@ -2,6 +2,7 @@ package uol.compass.microserviceb.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import uol.compass.microserviceb.model.Comment;
 import uol.compass.microserviceb.repositories.CommentRepository;
 
@@ -12,5 +13,10 @@ public class CommentService {
 
     public Comment save(Comment comment) {
         return commentRepository.save(comment);
+    }
+
+    @Transactional(readOnly = true)
+    public void deleteById(String id) {
+        commentRepository.deleteById(id);
     }
 }
