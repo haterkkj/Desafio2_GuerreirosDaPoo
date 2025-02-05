83a565dd91935da07581e21b5a5846ccec8abb85
joao.murari <joao_pichula@hotmail.com>
2025-02-05 15:55:55 -0300

    /feat: Exceptions added to findAll

--- microservice-b/src/main/java/uol/compass/microserviceb/web/exception/ApiExceptionHandler.java
+++ microservice-b/src/main/java/uol/compass/microserviceb/web/exception/ApiExceptionHandler.java
@@ -3,7 +3,6 @@ package uol.compass.microserviceb.web.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -30,4 +29,3 @@ public class ApiExceptionHandler {
     }
 
 }
-
