cd1cf9c435566ea053cb84bed6499d7f04c7cfcc
Thiago Ferreira dos Santos <thiagoferreira2004f@gmail.com>
2025-02-05 10:32:45 -0300

    /feat: implementing update and findById method

--- microservice-b/src/main/java/uol/compass/microserviceb/services/CommentService.java
+++ microservice-b/src/main/java/uol/compass/microserviceb/services/CommentService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import uol.compass.microserviceb.model.Comment;
 import uol.compass.microserviceb.repositories.CommentRepository;
+update import uol.compass.microserviceb.web.dto.CommentUpdateDTO;
 
 @RequiredArgsConstructor
 @Service
@@ -19,4 +20,18 @@ public class CommentService {
     public void deleteById(String id) {
         commentRepository.deleteById(id);
     }
+
+    @Transactional
+    public Comment update(Comment updatedComment) {
+        Comment foundComment = findById(updatedComment.getId());
+        if (foundComment != null) {
+            foundComment = updatedComment;
+        }
+        return commentRepository.save(foundComment);
+    }
+
+    public Comment findById(String id) {
+        return commentRepository.findById(id).orElse(null);
+    }
+
 }
