24daa1c50f9ce06924ff37f219d84ae0c170159a
CaioSifuentes <caiocesar.sifuentesbarcelos@gmail.com>
2025-02-06 15:50:04 -0300

    /feat: comment-service-a

--- microservice-a/src/main/java/uol/compass/microservicea/services/CommentService.java
+++ microservice-a/src/main/java/uol/compass/microservicea/services/CommentService.java
@@ -3,12 +3,17 @@ package uol.compass.microservicea.services;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import uol.compass.microservicea.clients.CommentClient;
+import uol.compass.microservicea.model.Comment;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Service
 public class CommentService {
     private final CommentClient client;
 
-
+    public List<Comment> getCommentsByPostId(String postId) {
+        return client.getCommentsByPostId(postId);
+    }
 
 }
