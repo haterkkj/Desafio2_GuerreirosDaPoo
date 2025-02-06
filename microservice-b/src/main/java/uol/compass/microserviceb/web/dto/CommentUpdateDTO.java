a8106c6b7359da18603352bf43216b0635c50eb3
CaioSifuentes <caiocesar.sifuentesbarcelos@gmail.com>
2025-02-06 17:13:07 -0300

    /feat: comment-validations

--- microservice-b/src/main/java/uol/compass/microserviceb/web/dto/CommentUpdateDTO.java
+++ microservice-b/src/main/java/uol/compass/microserviceb/web/dto/CommentUpdateDTO.java
@@ -1,10 +1,17 @@
 package uol.compass.microserviceb.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class CommentUpdateDTO {
+    @NotBlank
+    @Size(min = 2, max = 180)
     private String name;
+
+    @NotBlank
+    @Size(min = 3, max = 1080)
     private String body;
 }
