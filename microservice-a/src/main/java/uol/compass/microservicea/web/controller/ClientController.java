058493ed75f8132d314d4ac050e4d28915a2afb2
CaioSifuentes <caiocesar.sifuentesbarcelos@gmail.com>
2025-02-06 15:50:20 -0300

    /feat: comment-controller-a

--- microservice-a/src/main/java/uol/compass/microservicea/web/controller/ClientController.java
+++ microservice-a/src/main/java/uol/compass/microservicea/web/controller/ClientController.java
@@ -1,9 +1,15 @@
 package uol.compass.microservicea.web.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uol.compass.microservicea.model.Comment;
 import uol.compass.microservicea.services.CommentService;
+import uol.compass.microservicea.web.dto.CommentResponseDTO;
+import uol.compass.microservicea.web.dto.mapper.CommentMapper;
 
 import java.util.List;
 
@@ -13,5 +19,12 @@ import java.util.List;
 public class ClientController {
     private final CommentService service;
 
+    @GetMapping("/{postId}/comments")
+    public ResponseEntity<List<CommentResponseDTO>> getPosts(@PathVariable String postId) {
+        List<Comment> comments = service.getCommentsByPostId(postId);
+        List<CommentResponseDTO> response = CommentMapper.fromListCommentToListDto(comments);
+
+        return ResponseEntity.ok().body(response);
+    }
 
 }
