e21c96a2d1ca56ac41eee242151a3deb3e2f0a7e
joao.murari <joao_pichula@hotmail.com>
2025-02-06 17:34:03 -0300

    /feat: update-post-tests

--- microservice-b/src/main/java/uol/compass/microserviceb/web/dto/UpdatePostDTO.java
+++ microservice-b/src/main/java/uol/compass/microserviceb/web/dto/UpdatePostDTO.java
@@ -2,12 +2,12 @@ package uol.compass.microserviceb.web.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
-import uol.compass.microserviceb.model.Post;
 
-@Getter @Setter
+@Getter
+@Setter
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UpdatePostDTO {
     private String title;
     private String body;
-
 }
