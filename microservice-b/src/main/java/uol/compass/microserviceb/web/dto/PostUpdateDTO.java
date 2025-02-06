c2a9084d24540871bd4edcd9085342bf85dc678c
CaioSifuentes <caiocesar.sifuentesbarcelos@gmail.com>
2025-02-06 17:12:49 -0300

    /refact: renamed-post-update-dto

--- microservice-b/src/main/java/uol/compass/microserviceb/web/dto/PostUpdateDTO.java
+++ microservice-b/src/main/java/uol/compass/microserviceb/web/dto/PostUpdateDTO.java
@@ -1,13 +1,18 @@
 package uol.compass.microserviceb.web.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
-import uol.compass.microserviceb.model.Post;
 
 @Getter @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class UpdatePostDTO {
+public class PostUpdateDTO {
+    @NotBlank
+    @Size(min = 3, max = 80)
     private String title;
+    @NotBlank
+    @Size(min = 3, max = 2080)
     private String body;
 
 }
