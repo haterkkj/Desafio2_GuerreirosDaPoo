48e6551310f391db2b73059f6aa96e184efc5816
CaioSifuentes <caiocesar.sifuentesbarcelos@gmail.com>
2025-02-06 15:49:16 -0300

    /feat: comment-dto-mapper-a

--- microservice-a/src/main/java/uol/compass/microservicea/web/dto/CommentResponseDTO.java
+++ microservice-a/src/main/java/uol/compass/microservicea/web/dto/CommentResponseDTO.java
@@ -1,5 +1,11 @@
 package uol.compass.microservicea.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter @Setter
 public class CommentResponseDTO {
     private String id;
     private String email;
---
+++ microservice-a/src/main/java/uol/compass/microservicea/web/dto/mapper/CommentMapper.java
@@ -0,0 +1,18 @@
+package uol.compass.microservicea.web.dto.mapper;
+
+import uol.compass.microservicea.model.Comment;
+import uol.compass.microservicea.web.dto.CommentResponseDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CommentMapper {
+    public static CommentResponseDTO fromCommentToDto(Comment comment) {
+        return new CommentResponseDTO(comment.getId(), comment.getEmail(), comment.getName(), comment.getBody());
+    }
+
+    public static List<CommentResponseDTO> fromListCommentToListDto(List<Comment> posts) {
+        return posts.stream().map(CommentMapper::fromCommentToDto).collect(Collectors.toList());
+    }
+}
+
