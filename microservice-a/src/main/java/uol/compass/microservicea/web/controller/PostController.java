7d8f0189cd6669ba31521d5f62eaa20b22fa3cad
Thiago Ferreira dos Santos <thiagoferreira2004f@gmail.com>
2025-02-06 15:29:23 -0300

    /feat: implementing get post by id method and endpoint

--- microservice-a/src/main/java/uol/compass/microservicea/web/controller/PostController.java
+++ microservice-a/src/main/java/uol/compass/microservicea/web/controller/PostController.java
@@ -3,9 +3,9 @@ package uol.compass.microservicea.web.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uol.compass.microservicea.clients.PostClient;
 import uol.compass.microservicea.model.Post;
 import uol.compass.microservicea.services.PostService;
 
@@ -21,6 +21,12 @@ public class PostController {
     public ResponseEntity<List<Post>> getPosts() {
         List<Post> posts = postService.getPosts();
         return ResponseEntity.ok().body(posts);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable String id) {
+        Post post = postService.getPostById(id);
+        return ResponseEntity.ok().body(post);
     }
 
 }
