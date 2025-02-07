de21e0fa8e225253b732b0c02c5cb5b6f430963c
Thiago Ferreira dos Santos <thiagoferreira2004f@gmail.com>
2025-02-07 16:24:18 -0300

    /refactor: changing mapping logic

--- microservice-b/src/main/java/uol/compass/microserviceb/web/dto/mapper/PostMapper.java
+++ microservice-b/src/main/java/uol/compass/microserviceb/web/dto/mapper/PostMapper.java
@@ -1,8 +1,6 @@
 package uol.compass.microserviceb.web.dto.mapper;
 
-import uol.compass.microserviceb.model.Comment;
 import uol.compass.microserviceb.model.Post;
-import uol.compass.microserviceb.web.dto.CommentResponseDTO;
 import uol.compass.microserviceb.web.dto.PostResponseDTO;
 
 import java.util.List;
@@ -11,14 +9,10 @@ import java.util.stream.Collectors;
 
 public class PostMapper {
     public static PostResponseDTO fromPostToDto(Post post) {
-        return new PostResponseDTO(post.getId(), post.getTitle(), post.getBody(), fromCommentToDto(post.getComments()));
+        return new PostResponseDTO(post.getId(), post.getTitle(), post.getBody(), CommentMapper.fromListCommentToListDto(post.getComments()));
     }
 
     public static List<PostResponseDTO> fromListPostToListDto(List<Post> posts) {
         return posts.stream().map(PostMapper::fromPostToDto).collect(Collectors.toList());
-    }
-
-    private static List<CommentResponseDTO> fromCommentToDto(List<Comment> comments) {
-        return comments.stream().map(CommentResponseDTO::toDto).collect(Collectors.toList());
     }
 }
