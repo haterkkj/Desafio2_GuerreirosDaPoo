48e4b71068bf0f5f96e2eff562b731f6009117f8
Thiago Ferreira dos Santos <thiagoferreira2004f@gmail.com>
2025-02-07 15:24:57 -0300

    /feat: capturing MethodArgumentNotValidException from feign clients

--- microservice-a/src/main/java/uol/compass/microservicea/config/FeignErrorDecoder.java
+++ microservice-a/src/main/java/uol/compass/microservicea/config/FeignErrorDecoder.java
@@ -4,14 +4,22 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import uol.compass.microservicea.exceptions.EntityNotFoundException;
 import uol.compass.microservicea.exceptions.FeignClientException;
+import uol.compass.microservicea.exceptions.MethodArgumentNotValidException;
+import uol.compass.microservicea.web.dto.CommentCreateDTO;
+import uol.compass.microservicea.web.dto.PostCreateDTO;
+import uol.compass.microservicea.web.dto.PostUpdateDTO;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class FeignErrorDecoder implements ErrorDecoder {
 
@@ -23,6 +31,7 @@ public class FeignErrorDecoder implements ErrorDecoder {
             JsonNode errorNode = objectMapper.readTree(response.body().asInputStream());
 
             String message = errorNode.has("message") ? errorNode.get("message").asText() : "Unknown error";
+            String method = errorNode.has("method") ? errorNode.get("method").asText() : "unknown";
             String path = errorNode.has("path") ? errorNode.get("path").asText() : "N/A";
             int status = response.status();
 
@@ -33,6 +42,8 @@ public class FeignErrorDecoder implements ErrorDecoder {
                     return new RuntimeException(message);
                 case BAD_REQUEST:
                     return new IllegalArgumentException(message);
+                case UNPROCESSABLE_ENTITY:
+                        return buildMethodArgumentNotValidException(errorNode, message, path, method);
                 default:
                     return new FeignClientException(status, message, path);
             }
@@ -40,5 +51,40 @@ public class FeignErrorDecoder implements ErrorDecoder {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private MethodArgumentNotValidException buildMethodArgumentNotValidException(JsonNode errorNode, String message, String path, String method) {
+        JsonNode errorsNode = errorNode.get("errors");
+
+        BindingResult bindingResult = getBindingResult(path, method);
+
+        errorsNode.fields().forEachRemaining(field -> {
+            String fieldKey = field.getKey();
+            String errorMessage = field.getValue().asText();
+            bindingResult.addError(new FieldError("requestBody", fieldKey, errorMessage));
+        });
+
+        return new MethodArgumentNotValidException(message, HttpStatus.UNPROCESSABLE_ENTITY, bindingResult);
+    }
+
+    private static BindingResult getBindingResult(String path, String method) {
+        BindingResult bindingResult = new BeanPropertyBindingResult(new Object(), "requestBody");
+
+        if (method.equals("POST")) {
+            if(path.contains("/comments")) {
+                bindingResult = new BeanPropertyBindingResult(new CommentCreateDTO(null, null, null, null), "requestBody");
+            } else {
+                bindingResult = new BeanPropertyBindingResult(new PostCreateDTO(), "requestBody");
+            }
+        }
+
+        if (method.equals("PUT")) {
+            if(path.contains("/comments")) {
+                // BindingResult reassigned to new binding result with Object CommentUpdateDTO
+            } else {
+                bindingResult = new BeanPropertyBindingResult(new PostUpdateDTO(), "requestBody");
+            }
+        }
+        return bindingResult;
     }
 }
