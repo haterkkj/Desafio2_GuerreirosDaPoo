094fc8bb43f6835df5a9b661e15c0a1aaa8e636e
CaioSifuentes <caiocesar.sifuentesbarcelos@gmail.com>
2025-02-08 20:10:15 -0300

    /tests: create-comment-test

--- microservice-b/src/main/java/uol/compass/microserviceb/web/dto/CommentCreateDTO.java
+++ microservice-b/src/main/java/uol/compass/microserviceb/web/dto/CommentCreateDTO.java
@@ -3,11 +3,12 @@ package uol.compass.microserviceb.web.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import uol.compass.microserviceb.model.Comment;
 
-@Getter @Setter
+@Getter @Setter @AllArgsConstructor
 public class CommentCreateDTO {
     @NotBlank
     @Email(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Email must be valid")
@@ -18,7 +19,7 @@ public class CommentCreateDTO {
     private String name;
 
     @NotBlank
-    @Size(min = 3, max = 2080)
+    @Size(min = 3, max = 1080)
     private String body;
 
     public Comment toComment() {
