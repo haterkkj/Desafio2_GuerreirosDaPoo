5d400101381cc65056bee3ce3c57579170f293da
Thiago Ferreira dos Santos <thiagoferreira2004f@gmail.com>
2025-02-08 20:33:38 -0300

    /refactor: adding no args constructor

--- microservice-a/src/main/java/uol/compass/microservicea/web/dto/CommentUpdateDTO.java
+++ microservice-a/src/main/java/uol/compass/microservicea/web/dto/CommentUpdateDTO.java
@@ -2,8 +2,10 @@ package uol.compass.microservicea.web.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
 public class CommentUpdateDTO {
--- microservice-b/src/main/java/uol/compass/microserviceb/web/dto/CommentUpdateDTO.java
+++ microservice-b/src/main/java/uol/compass/microserviceb/web/dto/CommentUpdateDTO.java
@@ -2,9 +2,13 @@ package uol.compass.microserviceb.web.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter @Setter
 public class CommentUpdateDTO {
     @NotBlank
