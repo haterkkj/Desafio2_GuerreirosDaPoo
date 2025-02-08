d112c7c01117f70f617dafcd8db4ca464404067f
Felipe-Meneguzz1 <lipemeneguzzi@gmail.com>
2025-02-07 23:06:07 -0300

    /Feat: should_GetCommentsByPostId_Successfully

--- microservice-a/src/test/java/uol/compass/microservicea/MicroserviceAApplicationTests.java
+++ microservice-a/src/test/java/uol/compass/microservicea/MicroserviceAApplicationTests.java
@@ -5,9 +5,17 @@ import org.mockito.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import uol.compass.microservicea.model.Comment;
 import uol.compass.microservicea.services.CommentService;
 import uol.compass.microservicea.web.controller.CommentController;
+import uol.compass.microservicea.web.dto.CommentResponseDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
@@ -30,4 +38,18 @@ class MicroserviceAApplicationTests {
         comment = new Comment("123", "email@test.com", "Test Name", "Test Body");
     }
 
+    @Test
+    void should_GetCommentsByPostId_Successfully() {
+        List<Comment> comments = new ArrayList<>();
+        comments.add(comment);
+
+        when(commentService.getCommentsByPostId("post123")).thenReturn(comments);
+
+        ResponseEntity<List<CommentResponseDTO>> response = commentController.getPosts("post123");
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(1, response.getBody().size());
+        verify(commentService, times(1)).getCommentsByPostId("post123");
+    }
 }
